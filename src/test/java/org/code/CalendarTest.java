62b0fabd8720cb1f0136c6339dc95021f7ba33e9
joakim <joakimtj@hiof.no>
2022-10-26 16:06:40 +0200

    Tests are now being run on push. Renamed test-functions.

--- src/test/java/org/code/CalendarTest.java
+++ src/test/java/org/code/CalendarTest.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.Test;
 class CalendarTest {
 
     @Test
-    void notDivisibleBy4Test() {
+    void testNotDivisibleBy4() {
         // 2007 is not divisible by 4.
         Assertions.assertFalse(Calendar.isLeapYear(2007));
     }
     @Test
-    void divisibleBy4ButNot100Test() {
+    void testDivisibleBy4ButNot100() {
         // 2004 is divisible by 4 but not 100.
         Assertions.assertTrue(Calendar.isLeapYear(2004));
     }
     @Test
-    public void divisibleBy400Test() {
+    public void testDivisibleBy400() {
         // 2000 is divisible by 4, 100 and 400..s
         Assertions.assertTrue(Calendar.isLeapYear(2000));
     }
     @Test
-    void divisibleBy100ButNot400Test() {
+    void testDivisibleBy100ButNot400() {
         Assertions.assertFalse(Calendar.isLeapYear(1900));
     }
 }
