9303c6a8cef10d184b918bce0a29718404ce3fc7
joakim <joakimtj@hiof.no>
2022-10-26 15:28:56 +0200

    Work?

--- src/test/java/org/fuck/CalendarTest.java
+++ src/test/java/org/fuck/CalendarTest.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.Test;
 class CalendarTest {
 
     @Test
-    void testNotDivisibleBy4() {
+    void notDivisibleBy4Test() {
         // 2007 is not divisible by 4.
         Assertions.assertFalse(Calendar.isLeapYear(2007));
     }
     @Test
-    void testDivisibleBy4ButNot100() {
+    void divisibleBy4ButNot100Test() {
         // 2004 is divisible by 4 but not 100.
         Assertions.assertTrue(Calendar.isLeapYear(2004));
     }
     @Test
-    public void testDivisibleBy400() {
+    public void divisibleBy400Test() {
         // 2000 is divisible by 4, 100 and 400..
         Assertions.assertTrue(Calendar.isLeapYear(2000));
     }
     @Test
-    void testDivisibleBy100ButNot400() {
+    void divisibleBy100ButNot400Test() {
         Assertions.assertFalse(Calendar.isLeapYear(1900));
     }
 }
